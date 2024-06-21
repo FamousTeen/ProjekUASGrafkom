cdb6444bb524298c73f0645b8467108a738cbbf1
FamousTeen <125239075+FamousTeen@users.noreply.github.com>
2024-06-21 15:48:40 +0700

    zoom in + orbit done

--- player.js
+++ player.js
@@ -1,133 +1,127 @@
 import * as THREE from "three";
-import {FBXLoader} from 'three/addons/loaders/FBXLoader.js';
+import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
 
-export class Player{
+export class Player {
 
-    constructor(camera, controller, scene, speed){
+    constructor(camera, controller, scene, speed) {
         this.camera = camera;
         this.controller = controller;
         this.scene = scene;
         this.speed = speed;
         this.state = "idle";
-        this.rotationVector = new THREE.Vector3(0,0,0);
+        this.rotationVector = new THREE.Vector3(0, 0, 0);
         this.animations = {};
         this.lastRotation = 0;
 
-        this.camera.setup(new THREE.Vector3(0,0,0), this.rotationVector);
-
-        // this.mesh = new THREE.Mesh(
-        //     new THREE.BoxGeometry(1,1,1),
-        //     new THREE.MeshPhongMaterial({color: 0xFF1111})
-        // );
-        // this.scene.add(this.mesh);
-        // this.mesh.castShadow = true;
-        // this.mesh.receiveShadow = true;
+        this.camera.setup(new THREE.Vector3(0, 0, 0), this.rotationVector, this.controller.scaleZ);
 
         this.loadModel();
     }
 
-    loadModel(){
+    loadModel() {
         var loader = new FBXLoader();
         loader.setPath('./resources_3person/Knight/');
         loader.load('Great Sword Idle.fbx', (fbx) => {
             fbx.scale.setScalar(0.01);
             fbx.traverse(c => {
-              c.castShadow = true;
+                c.castShadow = true;
             });
             this.mesh = fbx;
             this.scene.add(this.mesh);
-            this.mesh.rotation.y += Math.PI/2;
+            this.mesh.rotation.y += Math.PI / 2;
 
             this.mixer = new THREE.AnimationMixer(this.mesh);
 
             var onLoad = (animName, anim) => {
                 const clip = anim.animations[0];
                 const action = this.mixer.clipAction(clip);
-          
+
                 this.animations[animName] = {
-                  clip: clip,
-                  action: action,
+                    clip: clip,
+                    action: action,
                 };
             };
 
             const loader = new FBXLoader();
             loader.setPath('./resources_3person/Knight/');
             loader.load('Great Sword Idle.fbx', (fbx) => { onLoad('idle', fbx) });
-            
+
             loader.load('Great Sword Run.fbx', (fbx) => { onLoad('run', fbx) });
 
             loader.load('Great Sword Jump.fbx', (fbx) => { onLoad('jump', fbx) });
+
         });
+
     }
 
-    update(dt){
-        if(this.mesh && this.animations){
+    update(dt) {
+        if (this.mesh && this.animations) {
             this.lastRotation = this.mesh.rotation.y;
-            var direction = new THREE.Vector3(0,0,0);
+            var direction = new THREE.Vector3(0, 0, 0);
 
-            if(this.controller.keys['forward']){
+            if (this.controller.keys['forward']) {
                 direction.x = 1;
-                this.mesh.rotation.y = Math.PI/2;
+                this.mesh.rotation.y = Math.PI / 2;
             }
-            if(this.controller.keys['backward']){
+            if (this.controller.keys['backward']) {
                 direction.x = -1;
-                this.mesh.rotation.y = -Math.PI/2;
+                this.mesh.rotation.y = -Math.PI / 2;
             }
-            if(this.controller.keys['left']){
+            if (this.controller.keys['left']) {
                 direction.z = -1;
                 this.mesh.rotation.y = Math.PI;
             }
-            if(this.controller.keys['right']){
+            if (this.controller.keys['right']) {
                 direction.z = 1;
                 this.mesh.rotation.y = 0;
             }
-            if(this.controller.keys['space']){
+            if (this.controller.keys['space']) {
                 direction.y = 1;
                 this.mesh.position.y = 1;
             }
-            else if(!(this.controller.keys['space'])){
+            else if (!(this.controller.keys['space'])) {
                 direction.y = 0;
                 this.mesh.position.y = -1;
             }
 
             this.lastRotation = this.mesh.rotation.y;
-            // console.log(direction.y);
+            console.log(direction.y)
 
-            if(direction.y > 0){
-                if(this.animations['jump']){
-                    if(this.state != "jump"){
+            if (direction.y > 0) {
+                if (this.animations['jump']) {
+                    if (this.state != "jump") {
                         this.mixer.stopAllAction();
                         this.state = "jump";
-                    } 
+                    }
                     this.mixer.clipAction(this.animations['jump'].clip).play();
                 }
             }
-            else if(!(direction.y > 0)){
+            else if (!(direction.y > 0)) {
                 if (this.state != "run") {
-                    if(this.animations['idle']){
-                        if(this.state != "idle"){
+                    if (this.animations['idle']) {
+                        if (this.state != "idle") {
                             this.mixer.stopAllAction();
                             this.state = "idle";
-                        } 
+                        }
                         this.mixer.clipAction(this.animations['idle'].clip).play();
                     }
                 }
             }
 
-            if(direction.x == 0 && direction.z == 0){
+            if (direction.x == 0 && direction.z == 0) {
                 if (this.state != "jump") {
-                    if(this.animations['idle']){
-                        if(this.state != "idle"){
+                    if (this.animations['idle']) {
+                        if (this.state != "idle") {
                             this.mixer.stopAllAction();
                             this.state = "idle";
-                        } 
+                        }
                         this.mixer.clipAction(this.animations['idle'].clip).play();
                     }
                 }
             }
-            else if (!(direction.x == 0) || !(direction.z == 0)){
-                if(this.animations['run']){
-                    if(this.state != "run"){
+            else if (!(direction.x == 0) || !(direction.z == 0)) {
+                if (this.animations['run']) {
+                    if (this.state != "run") {
                         this.mixer.stopAllAction();
                         this.state = "run";
                     }
@@ -135,76 +129,75 @@ export class Player{
                 }
             }
 
-            if(this.controller.mouseDown)
-            {
+            if (this.controller.mouseDown) {
                 var dtMouse = this.controller.deltaMousePos;
                 dtMouse.x = dtMouse.x / Math.PI;
                 dtMouse.y = dtMouse.y / Math.PI;
 
                 this.rotationVector.y += dtMouse.x * dt * 100;
                 this.rotationVector.z += dtMouse.y * dt * 100;
+
             }
 
-            var forwardVector = new THREE.Vector3(1,0,0);
-            var rightVector = new THREE.Vector3(0,0,1);
-            var upVector = new THREE.Vector3(0,1,0);
-            forwardVector.applyAxisAngle(new THREE.Vector3(0,1,0), this.rotationVector.y);
-            rightVector.applyAxisAngle(new THREE.Vector3(0,1,0), this.rotationVector.y);
+            var forwardVector = new THREE.Vector3(1, 0, 0);
+            var rightVector = new THREE.Vector3(0, 0, 1);
+            var upVector = new THREE.Vector3(0, 1, 0);
+            forwardVector.applyAxisAngle(new THREE.Vector3(0, 1, 0), this.rotationVector.y);
+            rightVector.applyAxisAngle(new THREE.Vector3(0, 1, 0), this.rotationVector.y);
 
             this.mesh.position.add(forwardVector.multiplyScalar(dt * this.speed * direction.x));
             this.mesh.position.add(rightVector.multiplyScalar(dt * this.speed * direction.z));
             this.mesh.position.add(upVector.multiplyScalar(dt * this.speed * direction.y));
 
-            // this.camera.positionOffSet.x = this.controller.scrollPosition;
+            this.camera.setup(this.mesh.position, this.rotationVector, this.controller.scaleZ);
 
-            this.camera.setup(this.mesh.position, this.rotationVector);
-
-            if(this.mixer){
+            if (this.mixer) {
                 this.mixer.update(dt);
             }
+
         }
     }
+
 }
 
+export class PlayerController {
 
-export class PlayerController{
-    constructor(){
+    constructor() {
         this.keys = {
             "forward": false,
             "backward": false,
             "left": false,
             "right": false,
             "space": false
-        };
+        }
         this.mousePos = new THREE.Vector2();
         this.mouseDown = false;
+        this.scaleZ = 0;
         this.deltaMousePos = new THREE.Vector2();
-        this.scrollPosition = 0;
-
         document.addEventListener('keydown', (e) => this.onKeyDown(e), false);
         document.addEventListener('keyup', (e) => this.onKeyUp(e), false);
         document.addEventListener('mousemove', (e) => this.onMouseMove(e), false);
         document.addEventListener('mousedown', (e) => this.onMouseDown(e), false);
         document.addEventListener('mouseup', (e) => this.onMouseUp(e), false);
-        // document.addEventListener('scroll', (e) => this.onScroll(e), false);
+        document.addEventListener('wheel', (event) => this.onWheel(event), false);
     }
-    
-    onMouseDown(event){
+
+    onMouseDown(event) {
         this.mouseDown = true;
     }
-    onMouseUp(event){
+    onMouseUp(event) {
         this.mouseDown = false;
     }
-    onMouseMove(event){
+    onMouseMove(event) {
         var currentMousePos = new THREE.Vector2(
             (event.clientX / window.innerWidth) * 2 - 1,
             -(event.clientY / window.innerHeight) * 2 + 1
         );
-        this.deltaMousePos.addVectors(currentMousePos, this.mousePos.multiplyScalar(-1)); 
+        this.deltaMousePos.addVectors(currentMousePos, this.mousePos.multiplyScalar(-1));
         this.mousePos.copy(currentMousePos);
     }
-    onKeyDown(event){
-        switch(event.keyCode){
+    onKeyDown(event) {
+        switch (event.keyCode) {
             case "W".charCodeAt(0):
             case "w".charCodeAt(0):
                 this.keys['forward'] = true;
@@ -226,8 +219,8 @@ export class PlayerController{
                 break;
         }
     }
-    onKeyUp(event){
-        switch(event.keyCode){
+    onKeyUp(event) {
+        switch (event.keyCode) {
             case "W".charCodeAt(0):
             case "w".charCodeAt(0):
                 this.keys['forward'] = false;
@@ -247,31 +240,33 @@ export class PlayerController{
             case " ".charCodeAt(0):
                 this.keys['space'] = false;
                 break;
-        }    
+        }
     }
-    onScroll(event){
-        // event.preventDefault();
-        this.scrollPosition += event.deltaY;
+    onWheel(event) {
+        event.preventDefault();
+        // Adjust scaleZ based on wheel delta
+        this.scaleZ += Math.max(-1, Math.min(1, (event.wheelDelta || -event.detail)));
     }
 }
 
 
+
 export class ThirdPersonCamera {
-    constructor(camera, positionOffSet, targetOffSet){
+    constructor(camera, positionOffSet, targetOffSet) {
         this.camera = camera;
         this.positionOffSet = positionOffSet;
         this.targetOffSet = targetOffSet;
     }
 
-    setup(target, angle, scrollOffset){
-        var temp = new THREE.Vector3(0,0,0);
+    setup(target, angle, scaleZ) {
+        var temp = new THREE.Vector3(0, 0, 0);
         temp.copy(this.positionOffSet);
-        temp.x += scrollOffset; // Apply scroll offset to X-axis
-        temp.applyAxisAngle(new THREE.Vector3(angle.x,1,0), angle.y);
-        temp.applyAxisAngle(new THREE.Vector3(angle.y,0,1), angle.z);
+        temp.x += scaleZ; // Apply the scaleZ value here
+        temp.applyAxisAngle(new THREE.Vector3(0, 1, 0), angle.y);
+        temp.applyAxisAngle(new THREE.Vector3(0, 0, 1), angle.z);
         temp.addVectors(target, temp);
         this.camera.position.copy(temp);
-        temp = new THREE.Vector3(0,0,0);
+        temp = new THREE.Vector3(0, 0, 0);
         temp.addVectors(target, this.targetOffSet);
         this.camera.lookAt(temp);
     }
